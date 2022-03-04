ba185f0c18df84dea428301420e09b40d4a07018
viktornuts <viktonuts@gmail.com>
2022-03-04 13:47:22 +0500

    Done HW 18

--- src/test/java/HomeWork18.java
+++ src/test/java/HomeWork18.java
@@ -1,16 +1,8 @@
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.hamcrest.core.IsNull.notNullValue;
 
 public class HomeWork18 {
 
